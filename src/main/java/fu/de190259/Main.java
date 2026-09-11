2dabb5ad526294342864599118166b81fff38cdc
Đặng Công Hoàng Nhật <dangconghoangnhat@gmail.com>
2026-09-11 14:27:18 +0700

    TODO2: Add persistence.xml, update Main.java - EMF created successfully

--- src/main/java/fu/de190259/Main.java
+++ src/main/java/fu/de190259/Main.java
@@ -1,11 +1,12 @@
 package fu.de190259;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsf302FU");
+        System.out.println("EMF tao thanh cong!");
+        emf.close();
     }
 }
